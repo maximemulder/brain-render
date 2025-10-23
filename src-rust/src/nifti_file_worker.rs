a8b0017bf4997863cde45438733f56a4e382c512
Maxime Mulder <maxime-mulder@outlook.com>
2025-10-23 17:56:04 +0800

    read all slices

--- src-rust/src/nifti_file_worker.rs
+++ src-rust/src/nifti_file_worker.rs
@@ -6,43 +6,47 @@ use web_sys::File;
 
 use crate::{log, nifti_slice::Nifti2DSlice};
 
-// NOTE: A web file cannot be sent between threads.
-// In an ideal architecture, the file is kept in the web worker, and the main thread asynchronously
-// calls the web worker whenever it needs to read the file.
-// static NIFTI: Mutex<Option<GenericNiftiObject<StreamedNiftiVolume<Either<BufReader<WebSysFile>, GzDecoder<BufReader<WebSysFile>>>>>>> = Mutex::new(None);
+pub struct NiftiPoint3D {
+    pub x: u16,
+    pub y: u16,
+    pub z: u16,
+}
+
+pub struct NiftiFileWorkerState {
+    focal_point: NiftiPoint3D,
+    volumes: Vec<InMemNiftiVolume>,
+}
 
 thread_local! {
-    static NIFTI_SLICE: RefCell<Option<InMemNiftiVolume>> = RefCell::new(None);
+    static STATE: RefCell<Option<NiftiFileWorkerState>> = RefCell::new(None);
 }
 
 pub async fn read_file(file: File) {
     log!("Starting to read the NIfTI file.");
     let nifti = ReaderStreamedOptions::new().read_web_file(file).expect("Cannot read NIfTI");
     let mut volume = nifti.into_volume();
-    for _ in 0..50 {
-        volume.read_slice().expect("Could not read slice.");
+
+    let mut volumes = Vec::new();
+    let mut slices_counter = 0;
+    while volume.slices_left() != 0 {
+        volumes.push(volume.read_slice().expect("Could not read slice."));
+        slices_counter += 1;
     }
 
-    match volume.read_slice() {
-        Ok(slice) => {
-            {
-                NIFTI_SLICE.replace(Some(slice));
-                log!("Successfully read NIfTI slice, slices left: {}", volume.slices_left());
-            }
-            NIFTI_SLICE.with_borrow(|slice| log!("Cell is some: {}", slice.is_some()));
-        },
-        Err(error) => {
-            log!("Error while reading NIfTI slice: {:?}", error);
-        },
-    }
+    log!("Read {} NIfTI slices.", slices_counter);
+
+    STATE.replace(Some(NiftiFileWorkerState {
+        focal_point: NiftiPoint3D { x: 50, y: 50, z: 50 },
+        volumes,
+    }));
 }
 
 pub fn send_file() -> JsValue {
-    NIFTI_SLICE.with_borrow(|slice| {
-        let Some(slice) = slice else {
+    STATE.with_borrow(|state| {
+        let Some(state) = state else {
             return JsValue::NULL;
         };
 
-        Nifti2DSlice::from_volume(&slice).to_js()
+        Nifti2DSlice::from_volume(&state.volumes[50]).to_js()
     })
 }
